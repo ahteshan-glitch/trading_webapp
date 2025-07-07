540f383ab4fcf5b937de202cfa2080ae3379f7ea
Ahteshan <mdahteshan796@gmail.com>
2025-07-07 13:25:46 +0530

    new update

--- backend/index.js
+++ backend/index.js
@@ -18,12 +18,9 @@ const {
 const PORT = process.env.PORT || 5000;
 const URL = process.env.MONGO_URL;
 
-
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors());
-
-
 
 app.get("/allholdings", async (req, res) => {
   const allHoldings = await holdingModel.find({});
@@ -44,61 +41,7 @@ app.post("/newOrder", async (req, res) => {
   });
   console.log(newOrder);
   await newOrder.save();
-});
-
-app.post("/signup", async (req, res) => {
-  console.log("welcome to signup page");
-  const { username, email, password } = req.body;
-
-  const existingUser = await userModel.findOne({ username });
-  if (existingUser) {
-    return res.status(409).json({ message: "Username already exists" });
-  }
-const express = require("express");
-const mongoose = require("mongoose");
-const app = express();
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const cookieParser = require("cookie-parser");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-
-const {
-  holdingModel,
-  orderModel,
-  userModel,
-  positionModel,
-} = require("./models/model.js");
-
-const PORT = process.env.PORT || 5000;
-const URL = process.env.MONGO_URL;
-
-
-app.use(bodyParser.json());
-app.use(cookieParser());
-app.use(cors());
-
-
-
-app.get("/allholdings", async (req, res) => {
-  const allHoldings = await holdingModel.find({});
-  res.send(allHoldings);
-});
-
-app.get("/allpositions", async (req, res) => {
-  const allPositions = await positionModel.find({});
-  res.send(allPositions);
-});
-
-app.post("/newOrder", async (req, res) => {
-  const newOrder = new orderModel({
-    name: req.body.name,
-    qty: req.body.qty,
-    price: req.body.price,
-    mode: req.body.mode,
-  });
-  console.log(newOrder);
-  await newOrder.save();
+  res.status(200).json({ message: "Order placed successfully" });
 });
 
 app.post("/signup", async (req, res) => {
@@ -111,15 +54,7 @@ app.post("/signup", async (req, res) => {
   }
 
   bcrypt.hash(password, 10, async (err, hash) => {
-    if (err) return console.log(err);
-
-    const newUser = await userModel.create({
-      username,
-      email,
-      password: hash,
-    });
-  bcrypt.hash(password, 10, async (err, hash) => {
-    if (err) return console.log(err);
+    if (err) return res.status(500).json({ message: "Hashing error" });
 
     const newUser = await userModel.create({
       username,
@@ -165,48 +100,7 @@ app.post("/login", async (req, res) => {
     }
   });
 });
-    const token = jwt.sign({ email }, process.env.JWT_SECRET);
-    res.cookie("token", token);
-    res.status(200).json({
-      message: "Account created successfully",
-      token,
-      redirectUrl: process.env.BACKEND_URL,
-    });
-  });
-});
 
-app.post("/logout", (req, res) => {
-  res.clearCookie("token").send({ message: "Logged out" });
-});
-
-app.post("/login", async (req, res) => {
-  console.log("response received");
-  const { username, password } = req.body;
-
-  const user = await userModel.findOne({ username });
-  if (!user)
-    return res.status(401).json({ message: "Username not found" });
-
-  bcrypt.compare(password, user.password, (err, result) => {
-    if (err)
-      return res.status(500).json({ message: "Internal error" });
-
-    if (result) {
-      const token = jwt.sign({ email: user.email }, process.env.JWT_SECRET);
-      res.cookie("token", token);
-      return res.status(200).json({
-        token,
-        redirectUrl: process.env.BACKEND_URL,
-      });
-    } else {
-      return res.status(403).json({ message: "Invalid credentials" });
-    }
-  });
-});
-
-app.get("/", (req, res) => {
-  res.send("Welcome to the backend");
-});
 app.get("/", (req, res) => {
   res.send("Welcome to the backend");
 });
