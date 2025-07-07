a0358cdad22ed77fdc8b883330c0dc164559eec7
Ahteshan <mdahteshan796@gmail.com>
2025-07-07 12:22:38 +0530

    Update Login.jsx final changes

--- frontend/src/landing_page/login/Login.jsx
+++ frontend/src/landing_page/login/Login.jsx
@@ -7,7 +7,7 @@ function Login() {
     const handleSubmit = async (e) => {
       e.preventDefault();
       try {
-        const res = await axios.post("http://localhost:5000/login", userData);
+        const res = await axios.post(import.meta.env.VITE_LOGIN_URL, userData);
 
         localStorage.setItem("token", res.data.token);
         window.location.href = res.data.redirectUrl;
